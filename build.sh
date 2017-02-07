178916e7329809c97c10e17a659e9ba698452c64
Arnaud Kervern <akervern@nuxeo.com>
2017-02-07 13:34:03 +0100

    Rewrite che-workspace image to use Official Nuxeo's one

--- build.sh
+++ build.sh
@@ -1,73 +1,18 @@
 #!/bin/bash -ex
 
 PUSH=${PUSH:-false}
-DOCKER_IMAGE_BASE=${DOCKER_IMAGE_BASE:-"nuxeo/che-base"}
 DOCKER_IMAGE=${DOCKER_IMAGE:-"nuxeo/che-workspace"}
-
-QUICK=${QUICK:-false}
-# Latest version should be tired from connect TP
-NUXEO_VERSION=${1:-master}
-
-if [[ "${NUXEO_VERSION}" =~ ^([0-9\.]+)-SNAPSHOT$ ]] || [[ "${NUXEO_VERSION}" =~ ^master$ ]];
-then
-  # Snapshot version; use static to resolve MD5
-  NUXEO_MD5_URL=`curl -Ls -o /dev/null -w %{url_effective} http://community.nuxeo.com/static/latest-snapshot/nuxeo,tomcat,md5,${BASH_REMATCH[1]}`
-else
-  # Release version; use the CDN
-  # NUXEO_MD5_URL="http://cdn.nuxeo.com/nuxeo-${NUXEO_VERSION}/nuxeo-server-${NUXEO_VERSION}-tomcat.zip.md5"
-  # NUXEO_URL="http://cdn.nuxeo.com/nuxeo-${NUXEO_VERSION}/nuxeo-server-${NUXEO_VERSION}-tomcat.zip"
-  NUXEO_MD5_URL=`curl -Ls -o /dev/null -w %{url_effective} http://community.nuxeo.com/static/latest-release/nuxeo,tomcat,md5,${NUXEO_VERSION}`
-fi
-
-NUXEO_URL=${NUXEO_MD5_URL%.md5}
-NUXEO_MD5=`curl -sL "${NUXEO_MD5_URL}" | awk '{print \$1}'`
-if ! [[ "${NUXEO_MD5}" =~ ^[a-f0-9]{32}$ ]]; then
-  echo "Unable to read MD5 from version ${NUXEO_VERSION}"
-  exit 1
-fi;
-
-echo "Building CHE image for Nuxeo Server $NUXEO_VERSION..."
-echo "MD5: $NUXEO_MD5"
-echo "URL: $NUXEO_URL"
+DOCKER_TAG=${NUXEO_VERSION:-"temp"}
+NUXEO_VERSION=${NUXEO_VERSION:-"8.10"}
 
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 WORKDIR=$DIR/workdir
 
-SOURCE_FOLDER=$WORKDIR/nuxeo-sources
-SOURCE_LINK=$DIR/nuxeo-che/nuxeo
-
-M2_FOLDER=$WORKDIR/m2-$NUXEO_VERSION
-M2_LINK=$DIR/nuxeo-che/m2
-
 # Set -x after variables setting...
 set -x
 
-mkdir -p $WORKDIR
-docker build -t $DOCKER_IMAGE_BASE nuxeo-che-base
-! ${PUSH} || docker push $DOCKER_IMAGE_BASE
-
-# Checkout sources
-# Use the tag if version is not snapshot; takes the branch otherwise
-if [[ "${NUXEO_VERSION}" =~ ^([0-9\.]+)-SNAPSHOT$ ]];
-then
-  if [ `git branch --list ${BASH_REMATCH[1]}` ]; then
-    GIT_REF=${BASH_REMATCH[1]}
-  fi
-else
-  if [[ "${NUXEO_VERSION}" =~ ^([0-9\.]+)$ ]]; then
-    GIT_REF="release-$NUXEO_VERSION"
-  else
-    GIT_REF=${NUXEO_VERSION}
-  fi
-fi
-if [ -z $GIT_REF ]; then
-  echo "Unable to resolve a git reference for version: ${NUXEO_VERSION}"
-  exit 2
-fi
-
-_XX_NUXEO_GITREF=$GIT_REF _XX_NUXEO_URL=$NUXEO_URL _XX_NUXEO_MD5=$NUXEO_MD5 envsubst '$_XX_NUXEO_URL:$_XX_NUXEO_MD5:$_XX_NUXEO_GITREF' < $DIR/install_nuxeo.tpl.sh > $DIR/nuxeo-che/install_nuxeo.sh
+_XX_NUXEO_GITREF=release-${NUXEO_VERSION} envsubst '$_XX_NUXEO_GITREF' < $DIR/install_nuxeo.tpl.sh > $DIR/nuxeo-che/install_nuxeo.sh
 chmod +x $DIR/nuxeo-che/install_nuxeo.sh
-cd $DIR && docker build -t $DOCKER_IMAGE:$NUXEO_VERSION nuxeo-che
-! ${PUSH} || docker push $DOCKER_IMAGE:$NUXEO_VERSION
-
+cd $DIR && docker build -t $DOCKER_IMAGE:$DOCKER_TAG nuxeo-che
+! ${PUSH} || docker push $DOCKER_IMAGE:$DOCKER_TAG
 
--- install_nuxeo.tpl.sh
+++ install_nuxeo.tpl.sh
@@ -1,23 +1,23 @@
 #!/bin/bash -ex
 
-export NUXEO_MD5=$_XX_NUXEO_MD5
-export NUXEO_URL=$_XX_NUXEO_URL
 export NUXEO_GITREF=$_XX_NUXEO_GITREF
-export NUXEO_HOME=/opt/server
+export NUXEO_SOURCES=/opt/nuxeo/sources
 
+# Should already be done by original image
 # curl -fsSL "http://cdn.nuxeo.com/nuxeo-${NUXEO_VERSION}/nuxeo-server-${NUXEO_VERSION}-tomcat.zip" -o /tmp/nuxeo-distribution-tomcat.zip \
-curl -fsSL "${NUXEO_URL}" -o /tmp/nuxeo-distribution-tomcat.zip \
-    && echo "$NUXEO_MD5 /tmp/nuxeo-distribution-tomcat.zip" | md5sum -c - \
-    && mkdir -p /tmp/nuxeo-distribution $(dirname $NUXEO_HOME) \
-    && unzip -q -d /tmp/nuxeo-distribution /tmp/nuxeo-distribution-tomcat.zip \
-    && DISTDIR=$(/bin/ls /tmp/nuxeo-distribution | head -n 1) \
-    && sudo mv /tmp/nuxeo-distribution/$DISTDIR $NUXEO_HOME  \
-    && sed -i -e "s/^#?\(facelets.REFRESH_PERIOD=.*\)$/\1/g" $NUXEO_HOME/bin/nuxeo.conf \
-    && sed -i -e "s/^#?\(JAVA_OPTS=.*-Xdebug -Xrunjdwp.*\)$/\1/g" $NUXEO_HOME/bin/nuxeo.conf \
-    && rm -rf /tmp/nuxeo-distribution* \
-    && chmod +x $NUXEO_HOME/bin/*ctl $NUXEO_HOME/bin/*.sh
+# curl -fsSL "${NUXEO_URL}" -o /tmp/nuxeo-distribution-tomcat.zip \
+#     && echo "$NUXEO_MD5 /tmp/nuxeo-distribution-tomcat.zip" | md5sum -c - \
+#     && mkdir -p /tmp/nuxeo-distribution $(dirname $NUXEO_HOME) \
+#     && unzip -q -d /tmp/nuxeo-distribution /tmp/nuxeo-distribution-tomcat.zip \
+#     && DISTDIR=$(/bin/ls /tmp/nuxeo-distribution | head -n 1) \
+#     && sudo mv /tmp/nuxeo-distribution/$DISTDIR $NUXEO_HOME  \
+#     && sed -i -e "s/^#?\(facelets.REFRESH_PERIOD=.*\)$/\1/g" $NUXEO_HOME/bin/nuxeo.conf \
+#     && sed -i -e "s/^#?\(JAVA_OPTS=.*-Xdebug -Xrunjdwp.*\)$/\1/g" $NUXEO_HOME/bin/nuxeo.conf \
+#     && rm -rf /tmp/nuxeo-distribution* \
+#     && chmod +x $NUXEO_HOME/bin/*ctl $NUXEO_HOME/bin/*.sh
 
 # Ensure all userland directories have good rights
-sudo chown -R user:user /home/user/.m2/ /opt/
-su user -c "git clone https://github.com/nuxeo/nuxeo.git /opt/nuxeo && cd /opt/nuxeo && git checkout ${NUXEO_GITREF} && git clean -fd"
-cd /opt/nuxeo && su user -c "PATH=$PATH:$M2_HOME/bin mvn install process-test-classes -DskipTests"
+sudo mkdir -p /var/run/nuxeo
+sudo chown -R user:user /home/user/.m2/ /opt/ /var/run/nuxeo
+su user -c "git clone https://github.com/nuxeo/nuxeo.git ${NUXEO_SOURCES} && cd ${NUXEO_SOURCES} && git checkout ${NUXEO_GITREF} && git clean -fd"
+cd ${NUXEO_SOURCES} && su user -c "MAVEN_OPTS=\"-Xmx2g\" PATH=$PATH:$M2_HOME/bin mvn install process-test-classes -DskipTests"
